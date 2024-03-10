0f618c43dffd5226fe4d881f281deb96e1e3d7a1
Jadzilla <202320018@fit.edu.ph>
2024-03-10 12:00:19 +0800

    changes

--- test.cpp
+++ test.cpp
@@ -5,14 +5,6 @@ using namespace std;
 int main()
 {
     
-    HUIIUII
-    cout<<"1. Open a New Account";
-    cout<<"2. Deposit";
-    cout<<"3. Withdraw";
-    it's me, hi, i'm the problem it's me
-
-        hello hello
-        
 }
 
 
